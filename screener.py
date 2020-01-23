810622aa6702739f99653215ca7d99718df642cf
victor <victor.fesenko@ria.com>
2020-01-23 18:02:36 +0200

    commit

--- screener.py
+++ screener.py
@@ -20,14 +20,22 @@ class Screener():
 		curent_screen = {"width": int(screen[0]), "height": int(screen[1])}
 		image = pyautogui.screenshot(region=(0,0, curent_screen["width"], curent_screen["height"]))
 		image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+		self.image = image
+		return self.image
+
+	def createLight(self, top_horizont, bottom_horizont, left_vertical, right_vertical):
+		
 		print curent_screen
 		print image[1079]
 
 
 
-		cv2.imshow("Screenshot", imutils.resize(image, width=600))
-		# cv2.waitKey(5000)
-		cv2.imwrite("pic.png", image)
+
+
+		# cv2.imshow("Screenshot", imutils.resize(image, width=600))
+		# # cv2.waitKey(5000)
+		# cv2.imwrite("pic.png", image)
 
 screener = Screener()
 
