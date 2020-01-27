9c1310784d1a3692d749d702a6d063e45d11e92c
victor <victor.fesenko@ria.com>
2020-01-27 15:35:12 +0200

    scrren top

--- screener.py
+++ screener.py
@@ -22,12 +22,44 @@ class Screener():
 		image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
 		self.image = image
+		self.curent_screen = curent_screen
 		return self.image
 
-	def createLight(self, top_horizont, bottom_horizont, left_vertical, right_vertical):
-		
-		print curent_screen
-		print image[1079]
+	def createLight(self, top_horizont, bottom_horizont, left_vertical, right_vertical, width):
+			
+
+		top_horizont_led_count = self.curent_screen["width"]//top_horizont
+		top_horizont_led_left = self.curent_screen["width"]%top_horizont
+
+		top_led = []
+		current_led = 1
+		while current_led <= top_horizont_led_count :
+			red = 0
+			green = 0
+			blue = 0
+			pixel_count = 0
+			for x in range(top_horizont_led_count) :
+				for y in range(width) :
+					pixel_count += 1
+					red += self.image[y][x][0]
+					green += self.image[y][x][1]
+					blue += self.image[y][x][2]
+
+
+			top_led.append([red//pixel_count, green//pixel_count, blue//pixel_count]) 
+			current_led +=1
+
+
+		# for y in range(top_horizont_led) :
+		# 	for y in range(width) :
+
+
+
+		# print top_led
+		# print top_horizont_led_count, "  ", top_horizont_led_left
+
+		# print self.curent_screen
+		# print self.image[1079]
 
 
 
@@ -40,3 +72,4 @@ class Screener():
 screener = Screener()
 
 screener.get()	
+screener.createLight(13, 13, 13, 13, 15)
